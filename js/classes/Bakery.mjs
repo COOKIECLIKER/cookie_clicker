5e1348dc675fda58d97be6585b1bdb13af2f9e65
aminehadefdev <amine.hadef.dev@gmail.com>
2020-05-01 15:32:05 +0200

    add js bakery and building

---
+++ js/classes/Bakery.mjs
@@ -0,0 +1,49 @@
+class Bakery{
+    constructor(name = 'mal', cookies = 0, buildings = [], cookiesPerClick = 1, cookiesPerSecond = 0){
+        this._name = name
+        this._cookies = cookies
+        this._buildings = buildings
+        this._cookiesPerClick = cookiesPerClick
+        this._cookiesPerSecond = cookiesPerSecond
+    }
+    get name(){
+        return this._name
+    }
+    set name(newName){
+        this._name = newName
+    }
+
+    get cookies(){
+        return this._cookies
+    }
+    set cookies(newCookies){
+        this._cookies = newCookies
+    }
+
+    get buildings(){
+        return this._buildings
+    }
+    set buildings(newBuildings){
+        this._buildings = newBuildings
+    }
+
+    get cookiesPerClick(){
+        return this._cookiesPerClick
+    }
+    set cookiesPerClick(newCookiesPerClick){
+        this._cookiesPerClick = newCookiesPerClick
+    }
+
+    get cookiesPerSecond(){
+        return this._cookiesPerSecond
+    }
+    set cookiesPerSecond(newCookiesPerSecond){
+        this._cookiesPerSecond = newCookiesPerSecond
+    }
+}
+
+//let b = new Bakery('amine')
+//b.name = "leo"
+//console.log(b.name)
+
+//module.exports = Bakery
---
+++ js/classes/Building.mjs
@@ -0,0 +1,43 @@
+class Building{
+    constructor({name, description, cookiesPerSecond,cost}, number = 0){
+        this._name = name
+        this._description = description
+        this._number = number
+        this._cookiesPerSecond = cookiesPerSecond
+        this._cost = cost
+    }
+    get name(){
+        return this._name
+    }
+    set name(name){
+        this._name = name
+    }
+
+    get description(){
+        return this._description
+    }
+    set description(description){
+        this._description = description
+    }
+
+    get number(){
+        return this._number
+    }
+    set number(number){
+        this._number = number
+    }
+
+    get cookiesPerSecond(){
+        return this._cookiesPerSecond
+    }
+    set cookiesPerSecond(cookiesPerSecond){
+        this._cookiesPerSecond = cookiesPerSecond
+    }
+
+    get cost(){
+        return this._cost
+    }
+    set cost(cost){
+        this._cost = cost
+    }
+}
