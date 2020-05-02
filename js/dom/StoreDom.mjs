269a2c32bb5a73f8618cea6d2190cd193fdd3bcc
LEONARDINHO-DEV <leonardinho-dev@macbook-pro-de-leonardinho-dev.home>
2020-05-02 15:46:55 +0200

    ee

--- js/dom/StoreDom.mjs
+++ js/dom/StoreDom.mjs
@@ -1,7 +1,18 @@
 import Building from '../classes/Building.mjs'
 
+
+
 export default class StoreDom{
     constructor(){
-        console.log('caca')
+        document.getElementById("buildings").innerHTML =`<div id="building-cursor" class="locked disabled">
+        <div class="icon"></div>
+        <div class="name">Cursor</div>
+        <div class="cost">15</div>
+        <div class="number"></div>
+      </div>
+       ` 
+        
     }
+
+   
 }
