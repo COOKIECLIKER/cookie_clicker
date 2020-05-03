67350ae1010987f60ad8883c0607fe53695e8ce6
LEONARDINHO-DEV <leonardinho-dev@macbook-pro-de-leonardinho-dev.home>
2020-05-03 17:11:19 +0200

    sotreTuile 99% FISH

--- js/dom/StoreDom.mjs
+++ js/dom/StoreDom.mjs
@@ -6,8 +6,10 @@ export default class StoreDom{
     constructor(){
         document.getElementById("buildings").innerHTML =`<div id="building-cursor" class="locked disabled">
         <div class="icon"></div>
+        <div class="container">
         <div class="name">Cursor</div>
         <div class="cost">15</div>
+        </div>
         <div class="number"></div>
       </div>
        ` 
